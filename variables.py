3dd3efa3336b43d87ac897d934c8945661881c92
Fernando Ruiz <100306428+Fernando-Ruiz176@users.noreply.github.com>
2024-07-31 10:54:50 -0400

    Agregando variables iniciales

---
+++ variables.py
@@ -0,0 +1,26 @@
+#Variables tipo entero 
+a = 10
+b = 20
+print (a)
+print (b)
+
+#valiables flotantes
+c=1.5
+d=3.7
+suma= c + d
+print(suma)
+
+#variables tipo string
+nombre = "Juan"
+apellido = "Perez"
+fullname = nombre + " " + apellido
+print(fullname)
+
+#variables tipo booleano
+lampara=True
+print("La lampara esta encendida",lampara)
+lampara=False
+print("La lampara esta apagada",lampara)
+
+
+
